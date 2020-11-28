5dab157c5812ea4bd5a1bdf1c78b21006044ad7f
Git Submissions <gitsubmissions96@gmail.com>
2020-11-28 16:02:51 +0530

    A few comments

--- src/components/ColorViewer.js
+++ src/components/ColorViewer.js
@@ -7,6 +7,11 @@ export default function Products() {
     for (let r = 32; r > 0; r--)
       for (let g = 32; g > 0; g--)
         for (let b = 32; b > 0; b--) {
+          //There are 32 possibilites for shades of R, G and B respectively
+          //The values from the loop are multiplied by 8 and reduced by 1
+          //The sequence of color values can thus be summarized as n*8-1 where n begins with 1 and ends with 255
+          //Sequence is ==> 7,15,23,...255
+          //Went with decrementing the loop values because it looked prettier to me this way for some reason :-)
           setColorArray((colorArray) => [
             ...colorArray,
             { r: r * 8 - 1, g: g * 8 - 1, b: b * 8 - 1 },
